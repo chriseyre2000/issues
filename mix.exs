2f1ca495f7a62b1fb54f6c70b73bbbbde9240536
Christopher Eyre <christopher.eyre@team.pottermore.com>
2017-04-22 07:42:15 +0100

    Added missing files @chriseyre2000

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -15,7 +15,8 @@ defmodule Issues.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :httpoison]]
+
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +29,9 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      httpoison: "~> 0.9",
+      poison: "~> 2.2"
+    ]
   end
 end
